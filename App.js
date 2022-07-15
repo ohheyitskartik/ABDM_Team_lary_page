c5d0ee814c4d9bc8b039ed044262538d2c5378c0
Nishanth Bhat <nishanth.bhat@bfhl.in>
2022-07-15 13:14:14 +0530

    react query setup

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import MainStack from './app/routes/routes';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: (failureCount, error) => {
+                if (error.status === 404) return false;
+                if (failureCount < 3) return true;
+                return false;
+            },
+        },
+    },
+});
 
 const App = () => {
     return (
-        <NavigationContainer initialRouteName="Home">
-            <MainStack />
-        </NavigationContainer>
+        <SafeAreaView style={{ backgroundColor: 'transparent', flex: 1 }}>
+            <QueryClientProvider client={queryClient}>
+                <NavigationContainer initialRouteName="Home">
+                    <MainStack />
+                </NavigationContainer>
+            </QueryClientProvider>
+        </SafeAreaView>
     );
 };
 
