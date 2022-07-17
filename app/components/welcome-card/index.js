5b0b5ee4a59229e4d7f6f047320400d60da7d98f
Kartik Rana <kartikrana@kartik-mac.local>
2022-07-17 11:25:12 +0530

    modal added for file download

--- app/components/welcome-card/index.js
+++ app/components/welcome-card/index.js
@@ -1,12 +1,22 @@
-import { View } from 'react-native';
-import React from 'react';
+import { Modal, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import Lottie from 'lottie-react-native';
 import CharacterAnimation from '../../../assets/animations/character.json';
+import DownloadAnimation from '../../../assets/animations/download.json';
 import Text from '../text';
+import CustomIcon from '../icon';
 
 const WelcomeCard = ({ name }) => {
+    const [openModal, setOpenModal] = useState(false);
     return (
-        <View style={{ flexDirection: 'row', padding: 10, paddingTop: 0, alignItems: 'center' }}>
+        <View
+            style={{
+                flexDirection: 'row',
+                padding: 10,
+                paddingTop: 0,
+                alignItems: 'center',
+                width: '100%',
+            }}>
             <View
                 style={{
                     marginRight: 15,
@@ -34,6 +44,48 @@ const WelcomeCard = ({ name }) => {
                 </Text>
                 <Text color="#878787">Let's check your stats</Text>
             </View>
+            <View style={{ position: 'absolute', right: 0 }}>
+                <CustomIcon
+                    variant="ionicons"
+                    name="download"
+                    size={20}
+                    color="black"
+                    solid
+                    style={{ marginRight: 18 }}
+                />
+            </View>
+            <Modal visible={openModal}>
+                <View style={{ position: 'absolute', top: 60, right: 10 }}>
+                    <CustomIcon
+                        variant="ionicons"
+                        name="close"
+                        size={30}
+                        color="black"
+                        solid
+                        style={{ marginRight: 18 }}
+                    />
+                </View>
+                <View
+                    style={{
+                        flex: 1,
+                        padding: 15,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}>
+                    <Lottie
+                        source={DownloadAnimation}
+                        style={{
+                            height: 200,
+                            width: 200,
+                        }}
+                        autoPlay
+                        loop
+                    />
+                    <Text style={{ textAlign: 'center' }}>
+                        {'Processing your Health Data,\nthis might take a minute'}
+                    </Text>
+                </View>
+            </Modal>
         </View>
     );
 };
